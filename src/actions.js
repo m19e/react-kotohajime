9ea19ef0ddc5187b880355d7c1f39b84d277a323
m19e <nexus.me7290@gmail.com>
2020-05-04 00:53:22 +0900

    Add actions

---
+++ src/actions.js
@@ -0,0 +1,23 @@
+/**
+ * action types
+ */
+
+export const CLICK_SQUARE = "CLICK_SQUARE";
+export const JUMP_TO_PAST = "JUMP_TO_PAST";
+export const MAKE_UP_GAME = "MAKE_UP_GAME";
+
+/**
+ * action creaters
+ */
+
+export function clickSquare(index) {
+    return { type: CLICK_SQUARE, index };
+}
+
+export function jumpToPast(step) {
+    return { type: JUMP_TO_PAST, step };
+}
+
+export function makeUpGame(finish) {
+    return { type: MAKE_UP_GAME, finish };
+}
