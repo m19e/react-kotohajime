410d640c1886f9be11d1c4a74afc0da7bd8b78fd
m19e <nexus.me7290@gmail.com>
2020-05-03 18:43:23 +0900

    Show past moves

--- src/index.js
+++ src/index.js
@@ -140,6 +140,15 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ? "Go to move #" + move : "Go to game start";
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status, finish;
         if (winner) {
             status = "Winner: " + winner;
@@ -167,7 +176,7 @@ class Game extends React.Component {
                         <br />
                         {this.renderRematch(finish)}
                     </div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
